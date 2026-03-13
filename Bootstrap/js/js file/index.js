5f93ce09b60f493c1ce745f5fe602ff5303aabfb
Rukhsana batool <kashannabi123@gmail.com>
2026-03-13 15:04:20 +0500

    add a new file

---
+++ Bootstrap/js/js file/index.js
@@ -0,0 +1,51 @@
+Q=1
+// let numb=45;
+// if(numb>0){
+//     console.log("positive numb");
+
+// }
+// else if(numb<0){
+//     console.log("negative numb");
+// }
+// else if(numb==0){
+//     console.log("numb is equal to zero")
+// }
+// else{
+//     console.log("invalid")
+// }
+
+Q=2
+
+
+// let Personage=20;
+// if(Personage>=18){
+//     console.log("it is eligible for work")
+// }
+// else{
+//     console.log("it is not eligibale for work")
+// }
+
+
+Q=3
+
+// let vowel="b";
+// if(vowel=="a"||vowel=="e"||vowel=="i"||vowel=="o"||vowel=="u"){
+//     console.log("it is vowel");
+// }
+// else{
+//     console.log("it is a consonant")
+// }
+
+
+
+
+Q=4
+
+
+let num=55;
+if(num%5==0&& num%11==0){
+    console.log("number is divisible by 5 and 11")
+}
+else{
+    console.log("not divisible by 5 and 11")
+}
---
+++ Bootstrap/js/js file/index2.js
@@ -0,0 +1,99 @@
+
+
+// let oper="-";
+// switch (oper) {
+//     case "+":
+//     console.log("add");
+    
+//         break;
+//     case "-":
+//     console.log("sub");
+//        break;
+//     default:
+//      console.log("invalid");
+     
+// }
+// let age=29
+// let a=age>=18?"can vote":"cannot cost vote"
+// console.log(a);
+
+
+
+// Loop
+// type
+// for
+// while
+// do while
+// for each 
+// for in 
+// for at 
+// map 
+// reduce 
+// filter 
+
+
+// for(initilization , condition, incri/dec){
+//     block of code
+// }
+
+
+// for(let i=1;i<0;i++){
+//     console.log(`2 X ${i} = ${i*2}`);
+//     console.log("2" +"");
+    
+    
+// }
+// let b=1
+// let c=1
+// let sum=c+ ++c
+// console.log(sum);
+// let sm=b+b++
+// console.log(sm);
+
+
+
+// Array
+// let arr1=["admin","teacher","student"];
+// console.log(arr1[9]);
+// console.log(arr1[1]);
+// arr1.push("Editer")
+// arr1.push("staff")
+// console.log(arr1);
+// arr1.pop()
+// console.log(arr1);
+// arr1.shift()
+// console.log(arr1);
+// arr1.unshift("id")
+// console.log(arr1);
+
+// Function
+function run(){
+    console.log("backend is runing");
+    
+}
+run()
+let runing=()=>{
+
+
+      console.log("API is runing");
+      }
+      runing()
+      let sum=(a,b)=>{
+console.log(a+b);
+
+      }
+      sum(2,4)
+      let sums=(a,b)=> console.log(a+b);
+      sum(2,3)
+      let sm=(a,b)=>{
+        return a+b
+      }
+      
+      console.log(sm=(2,3));
+      let sms=(a,b)=> a+b;
+      console.log(sms(1,1));
+
+      let sq=a=> a*a;
+      console.log(sq(2));
+      
+      
