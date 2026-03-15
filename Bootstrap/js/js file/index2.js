34a5934b7a0977fe58e8db12558fb8dd5a2d027f
Rukhsana batool <kashannabi123@gmail.com>
2026-03-15 14:41:51 +0500

    add a new file

--- Bootstrap/js/js file/index2.js
+++ Bootstrap/js/js file/index2.js
@@ -1,21 +1,21 @@
 
 
-// let oper="-";
-// switch (oper) {
-//     case "+":
-//     console.log("add");
+let oper="-";
+switch (oper) {
+    case "+":
+    console.log("add");
     
-//         break;
-//     case "-":
-//     console.log("sub");
-//        break;
-//     default:
-//      console.log("invalid");
+        break;
+    case "-":
+    console.log("sub");
+       break;
+    default:
+     console.log("invalid");
      
-// }
-// let age=29
-// let a=age>=18?"can vote":"cannot cost vote"
-// console.log(a);
+}
+let age=29
+let a=age>=18?"can vote":"cannot cost vote"
+console.log(a);
 
 
 
@@ -37,36 +37,36 @@
 // }
 
 
-// for(let i=1;i<0;i++){
-//     console.log(`2 X ${i} = ${i*2}`);
-//     console.log("2" +"");
+for(let i=1;i<0;i++){
+    console.log(`2 X ${i} = ${i*2}`);
+    console.log("2" +"");
     
     
-// }
-// let b=1
-// let c=1
-// let sum=c+ ++c
-// console.log(sum);
-// let sm=b+b++
-// console.log(sm);
+}
+let b=1
+let c=1
+let sum=c+ ++c
+console.log(sum);
+let sm=b+b++
+console.log(sm);
 
 
 
 // Array
-// let arr1=["admin","teacher","student"];
-// console.log(arr1[9]);
-// console.log(arr1[1]);
-// arr1.push("Editer")
-// arr1.push("staff")
-// console.log(arr1);
-// arr1.pop()
-// console.log(arr1);
-// arr1.shift()
-// console.log(arr1);
-// arr1.unshift("id")
-// console.log(arr1);
+let arr1=["admin","teacher","student"];
+console.log(arr1[9]);
+console.log(arr1[1]);
+arr1.push("Editer")
+arr1.push("staff")
+console.log(arr1);
+arr1.pop()
+console.log(arr1);
+arr1.shift()
+console.log(arr1);
+arr1.unshift("id")
+console.log(arr1);
 
-// Function
+Function
 function run(){
     console.log("backend is runing");
     
@@ -96,4 +96,24 @@ console.log(a+b);
       let sq=a=> a*a;
       console.log(sq(2));
       
+      let arr=["iphone", "techno","opo","infinix ","redme"];
+      let product=arr.map(product=>product)
+      console.log(product);
+      let arr1=[1100,200,400,300,2000]
+      let  filt=arr1.filter(index=>index>400);
+      console.log(filt);
+      let search=arr.filter(search=>search.includes("opo"))
+      console.log(search);
+       let arr=["iphone", "techno","opo","infinix ","redme"];
+   let find=arr.find(find=>find=="Iphone")
+   console.log(find);
+
+
+
+
+   let arr1=[200 , 300 ,400,500]
+let reduce=arr1.reduce((sum ,price)=>sum+price,0)
+console.log(reduce);
+
+
       
--- js file/index.js
+++ js file/index.js
@@ -42,10 +42,10 @@ Q=3
 Q=4
 
 
-// let num=55;
-// if(num%5==0&& num%11==0){
-//     console.log("number is divisible by 5 and 11")
-// }
-// else{
-//     console.log("not divisible by 5 and 11")
-// }
+let num=55;
+if(num%5==0&& num%11==0){
+    console.log("number is divisible by 5 and 11")
+}
+else{
+    console.log("not divisible by 5 and 11")
+}
